665c98da977d17924b606914ab238305db1ea93b
Jennifer <jenniferpeterson@pursuit.org>
2024-04-10 16:49:40 -0400

    created the edit fx and exported it

--- queries/trips.js
+++ queries/trips.js
@@ -20,4 +20,87 @@ const getTripById = async (id) => {
   }
 };
 
-module.exports = { getAllTrips, getTripById };
+const createTrip = async (trip) => {
+  const {
+    user_id,
+    destination,
+    first_time,
+    start_date,
+    end_date,
+    budget,
+    total_cost,
+    climate,
+  } = trip;
+
+  try {
+    const newTrip = await db.one(
+      "INSERT INTO trips (user_id,destination,first_time,start_date,end_date,budget,total_cost,climate) VALUES($1, $2, $3, $4,$5, $6, $7, $8) RETURNING *",
+      [
+        user_id,
+        destination,
+        first_time,
+        start_date,
+        end_date,
+        budget,
+        total_cost,
+        climate,
+      ]
+    );
+    return newTrip;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteTripById = async (id) => {
+  try {
+    const deletedTrip = await db.one(
+      "DELETE FROM trips WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedTrip;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateTripById = async (id, trip) => {
+  const {
+    user_id,
+    destination,
+    first_time,
+    start_date,
+    end_date,
+    budget,
+    total_cost,
+    climate,
+  } = trip;
+
+  try {
+    const updatedTrip = await db.one(
+      "UPDATE trips SET user_id = $1,destination = $2,first_time = $3,start_date = $4,end_date = $5,budget = $6,total_cost = $7,climate = $8 WHERE id=$9 RETURNING *",
+      [
+        user_id,
+        destination,
+        first_time,
+        start_date,
+        end_date,
+        budget,
+        total_cost,
+        climate,
+        id,
+      ]
+    );
+    return updatedTrip;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAllTrips,
+  getTripById,
+  createTrip,
+  deleteTripById,
+  updateTripById,
+};
