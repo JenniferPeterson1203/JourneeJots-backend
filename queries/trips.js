1abb629fe7236c0f5232da64640712ae9d4194a2
Jennifer <jenniferpeterson@pursuit.org>
2024-04-10 14:30:57 -0400

    changed the trips queries so that it includes the number of entries

--- queries/trips.js
+++ queries/trips.js
@@ -2,8 +2,19 @@ const db = require("../db/dbConfig");
 
 const getAllTrips = async () => {
   try {
-    const allTrips = await db.any("SELECT * FROM trips");
+    const allTrips = await db.any(
+      "SELECT trips.*,COUNT(entries.trip_id) AS entry_count FROM trips LEFT JOIN entries ON trips.id = entries.trip_id GROUP BY trips.id"
+    );
     return allTrips;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getTripById = async (id) => {
+  try {
+    const oneTrip = await db.one("SELECT * FROM trips WHERE id=$1", id);
+    return oneTrip;
   } catch (error) {
     return error;
   }
