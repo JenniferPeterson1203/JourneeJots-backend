c8bf18c2a5f2359e9def1df4ce516ebb3c66388b
Jennifer <jenniferpeterson@pursuit.org>
2024-04-10 17:08:53 -0400

    created the function to create one journal entry

--- queries/entries.js
+++ queries/entries.js
@@ -1,5 +1,6 @@
 const db = require("../db/dbConfig");
 
+// function to access ALL entries
 const getAllEntries = async () => {
   try {
     const allEntries = await db.any("SELECT * FROM entries");
@@ -9,6 +10,7 @@ const getAllEntries = async () => {
   }
 };
 
+// function to access ONE specific entry
 const getEntryById = async (id) => {
   try {
     const oneEntry = await db.one("SELECT * FROM entries WHERE id=$1", id);
@@ -18,4 +20,19 @@ const getEntryById = async (id) => {
   }
 };
 
-module.exports = { getAllEntries, getEntryById };
+// function to create a new entry
+const createEntry = async (entries) => {
+  const { user_id, entry_date, trip_id, location, entry, total_spent } =
+    entries;
+  try {
+    const newEntry = await db.one(
+      "INSERT INTO entries (user_id,entry_date,trip_id,location,entry,total_spent) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [user_id, entry_date, trip_id, location, entry, total_spent]
+    );
+    return newEntry;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllEntries, getEntryById, createEntry };
