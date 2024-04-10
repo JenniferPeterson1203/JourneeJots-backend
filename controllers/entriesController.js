6714674674fe8bf0dec3fd337241298c9423626b
Jennifer <jenniferpeterson@pursuit.org>
2024-04-10 17:20:29 -0400

    imported the createEntry function and set up the route for creating an entry

--- controllers/entriesController.js
+++ controllers/entriesController.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const entries = express.Router();
 
-const { getAllEntries, getEntryById } = require("../queries/entries");
+const {
+  getAllEntries,
+  getEntryById,
+  createEntry,
+} = require("../queries/entries");
 
+// route to get ALL journal entries
 entries.get("/", async (req, res) => {
   try {
     const allEntries = await getAllEntries();
@@ -12,6 +17,7 @@ entries.get("/", async (req, res) => {
   }
 });
 
+// route to get ONE specific journal entry
 entries.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -25,4 +31,15 @@ entries.get("/:id", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
+// route to CREATE a journal entry
+entries.post("/", async (req, res) => {
+  try {
+    const newEntry = await createEntry(req.body);
+    res.status(200).json(newEntry);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = entries;
